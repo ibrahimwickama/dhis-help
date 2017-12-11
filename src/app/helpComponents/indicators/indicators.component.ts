aae42a63f0270cf35b6aa6f0817e405183ef2495
Ibrahim Wickama <ibrahimwickama@gmail.com>
2017-12-11 20:25:21 +0300

    add user management help guide

--- src/app/helpComponents/indicators/indicators.component.ts
+++ src/app/helpComponents/indicators/indicators.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-indicators',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./indicators.component.css']
 })
 export class IndicatorsComponent implements OnInit {
+
+  @Input() selection;
 
   constructor() { }
 
